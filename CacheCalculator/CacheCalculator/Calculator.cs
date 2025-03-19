8eed36403c89b60c5733ac9df14bc7fa0d15616c
SWoolfeek <50416553+SWoolfeek@users.noreply.github.com>
2025-03-19 12:57:46 +0100

    Calculation

---
+++ CacheCalculator/CacheCalculator/Calculator.cs
@@ -0,0 +1,73 @@
+namespace CacheCalculator;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Calculator
+{
+    private string _requestParams;
+    private string _secretKey;
+
+    public string Calculate()
+    {
+        SortRequestParams();
+        return GetMd5Hash(_requestParams + _secretKey);
+    }
+
+    public void SetParams(string inputRequestParams)
+    {
+        _requestParams = inputRequestParams;
+    }
+    
+    public void SetKey(string inputSecretKey)
+    {
+        _secretKey = inputSecretKey;
+    }
+
+    private void SortRequestParams()
+    {
+        Dictionary<string, string> parameterDictionary = new Dictionary<string, string>();
+        foreach (string parameter in _requestParams.Split('&'))
+        {
+            string[] splitedParameters = parameter.Split('=');
+            parameterDictionary[splitedParameters[0]] = splitedParameters[1];
+        }
+        
+        List<string> sortedKeys = parameterDictionary.Keys.ToList();
+        sortedKeys.Sort();
+        
+        if (sortedKeys.Count < 2)
+        {
+            return;
+        }
+
+        string result = "";
+
+        foreach (string key in sortedKeys)
+        {
+            result += key + "=" + parameterDictionary[key] + "&";
+        }
+
+        result = result.TrimEnd('&');
+        
+        Console.WriteLine(result);
+        _requestParams = result;
+    }
+
+    private string GetMd5Hash(string input)
+    {
+        using (MD5 md5 = MD5.Create())
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            // Convert byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hashBytes)
+            {
+                sb.Append(b.ToString("x2")); // Converts to lowercase hex
+            }
+            return sb.ToString();
+        }
+    }
+}
--- CacheCalculator/CacheCalculator/Program.cs
+++ CacheCalculator/CacheCalculator/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CacheCalculator;
+
 Console.WriteLine("Hello, World!");
+Calculator cCalculator = new Calculator();
+cCalculator.SetParams("secureLogin=ext_test1&options=GetFeatures");
+cCalculator.SetKey("testKey");
+Console.WriteLine("hash = " + cCalculator.Calculate());
